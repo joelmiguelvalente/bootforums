435a8fb15559d61d6f2eec33a1d8da21c29e2c34
mitchellurgero <mitchellurgero@gmail.com>
2016-06-05 18:16:12 -0500

    configurable registration captcha

--- config.php
+++ config.php
@@ -1,7 +1,23 @@
 <?php
 $config = array(
 	"title"=>"BootForums",
-	"desc"=>"Open Source Bootstrap themed forum for php 5.6+ - Flat file, no Database required!"
-	
+	"desc"=>"Open Source Bootstrap themed forum for php 5.6+ - Flat file, no Database required!",
+	"captcha" => array(
+    					'min_length' => 6,
+    					'max_length' => 8,
+    					'backgrounds' => array('backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png'),
+    					'fonts' => array('fonts/times_new_yorker.ttf'),
+    					'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
+    					'min_font_size' => 15,
+    					'max_font_size' => 20,
+    					'color' => '#666',
+    					'angle_min' => 0,
+    					'angle_max' => 20,
+    					'shadow' => true,
+    					'shadow_color' => '#fff',
+    					'shadow_offset_x' => -1,
+    					'shadow_offset_y' => 1
+					),
+	"ssl" => false
 	);
 ?>
--- register.php
+++ register.php
@@ -4,22 +4,7 @@ require("db.php");
 require("config.php");
 require("simple-php-captcha.php");
 
-$_SESSION['captcha'] = simple_php_captcha( array(
-    'min_length' => 6,
-    'max_length' => 8,
-    'backgrounds' => array('backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png'),
-    'fonts' => array('fonts/times_new_yorker.ttf'),
-    'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
-    'min_font_size' => 15,
-    'max_font_size' => 20,
-    'color' => '#666',
-    'angle_min' => 0,
-    'angle_max' => 20,
-    'shadow' => true,
-    'shadow_color' => '#fff',
-    'shadow_offset_x' => -1,
-    'shadow_offset_y' => 1
-));
+$_SESSION['captcha'] = simple_php_captcha( $config['captcha']);
 //Begin page
 include("header.php");
 echo '<div class="container">';
