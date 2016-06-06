2cd9160c57948f5da9da03d62651fb09d6c72782
mitchellurgero <mitchellurgero@gmail.com>
2016-06-06 15:55:34 -0500

    Make footer stick to bottom of browser window with some extra br padding for mobile devices.

--- footer.php
+++ footer.php
@@ -3,7 +3,14 @@ require("config.php");
 ?>
 <br />
 <br />
-<nav class="navbar navbar-inverse">
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
 <div class="container">
   <div class="container-fluid">
     <br /><p class="text-center" style="color:white"><?php echo $config['copyright']; ?></p>
