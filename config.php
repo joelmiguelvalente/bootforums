7129928dca43791393d89b393a797a54637111d0
mitchellurgero <mitchellurgero@gmail.com>
2016-06-10 20:22:46 -0500

    Added another theme called Flatly

--- config.php
+++ config.php
@@ -63,10 +63,10 @@ $config = array(
 	"announce" => "This forum is currently in beta, but please enjoy your stay!", //Announcement to show on home page. set to "" if you want to disable. Supports HTML if needed.
 	"copyright" => 'Powered by <a href="https://github.com/mitchellurgero/bootforums">BootForums</a><br />Copyright &copy; URGERO.ORG 2016', //Copyright footer, can be anything, don't be shy. (Copyright is centered in page.)
 	"footer" => '<a href="https://github.com/mitchellurgero/bootforums">Get BootForums from GitHub today!</a>', //Footer will be displayed under the copyright text, you can put links, or whatever you want in here.
-	"registration" => false, //Allow or disallow public registration (Disabled by default, for now, just change to true to enable.)
+	"registration" => true, //Allow or disallow public registration (Disabled by default, for now, just change to true to enable.)
 	"perPage" => 20, //Default amount of threads to show per page. (Index only.)
 	"perPageThread" => 10, //Default reply amount to show in each thread (View mode)
-	"allowNewThreads" => false, //Enable or disable the New Post button on index page, this will also disable cURL posts!
-	"theme" => "cosmo"//Select a theme: orig, cyborg, dark, journal, superhero, readable, or cosmo.
+	"allowNewThreads" => true, //Enable or disable the New Post button on index page, this will also disable cURL posts!
+	"theme" => "cosmo"//Select a theme: orig, cyborg, dark, journal, superhero, readable, flat, or cosmo.
 	);
 ?>
