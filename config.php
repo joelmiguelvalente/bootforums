b3702ac89785a1fee958b2354541baf6ec92ffaf
Richard Leishman <richard@webfwd.co.uk>
2016-11-25 16:50:34 +0000

    fix config

--- config.php
+++ config.php
@@ -16,57 +16,58 @@ Script is distributed with Open Source Licenses, do what you want with it. ;)
 finds this PHP script as useful as I do."
 
 */
+
 $config = array(
-	"admins" => array(""), //Username(s) of the account you want to be admin(**Register this account FIRST) This user will have access to locking threads, or deleting them even if they do not own it. Leave as just "" for no admin accounts.
-	"title"=>"BootForums", //Title for the forums
-	"desc"=>"Open Source Bootstrap themed forum for php 5.6+ - Flat file, no Database required!", //Description for the site in html (Not displayed on page, but in search engines.)
-	"home"=> "./", //Home page URL. to go back to home of forums: "./";
-	//Data folders must NOT be the same folders, please follow a similar structure to what I have below. (/path/to/data/users & /path/to/data/threads)
-	"user_data" => DATAROOT."/forum/users", //Folder to store user data in, make sure to give proper permissions (0744) and the owner of the folder must be apache's user (Or nginx's user)
-	"thread_data" => DATAROOT."/forum/threads", //Folder to store thread data in, make sure to give proper permissions (0744) and the owner of the folder must be apache's user (Or nginx's user)
-	"log" => DATAROOT."/forum/forums.log",
-	//Captcha Settings for REGISTRATION page
-	"captcha" => array( //Configure Captcha settings for registration.
-    					'min_length' => 4,
-    					'max_length' => 8,
-    					'backgrounds' => array('backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png'),
-    					'fonts' => array('fonts/times_new_yorker.ttf'),
-    					'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
-    					'min_font_size' => 15,
-    					'max_font_size' => 20,
-    					'color' => '#666',
-    					'angle_min' => 0,
-    					'angle_max' => 20,
-    					'shadow' => true,
-    					'shadow_color' => '#fff',
-    					'shadow_offset_x' => -1,
-    					'shadow_offset_y' => 1
-					),
-	//Captcha settings for LOGIN page
-	"captchaLoginForce" => true, //Forces captcha for ALL logins whether they got the password wrong or not.
-	"captchaLogin" => array( //Configure Captcha settings for login.
-    					'min_length' => 4,
-    					'max_length' => 6,
-    					'backgrounds' => array('backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png'),
-    					'fonts' => array('fonts/times_new_yorker.ttf'),
-    					'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
-    					'min_font_size' => 15,
-    					'max_font_size' => 20,
-    					'color' => '#666',
-    					'angle_min' => 0,
-    					'angle_max' => 20,
-    					'shadow' => true,
-    					'shadow_color' => '#fff',
-    					'shadow_offset_x' => -1,
-    					'shadow_offset_y' => 1
-					),
-	"ssl" => false, //Force an SSL connection for Forums
-	"announce" => "This forum is currently in beta, but please enjoy your stay!", //Announcement to show on home page. set to "" if you want to disable. Supports HTML if needed.
-	"copyright" => 'Powered by <a href="https://github.com/mitchellurgero/bootforums">BootForums</a><br />Copyright &copy; URGERO.ORG 2016', //Copyright footer, can be anything, don't be shy. (Copyright is centered in page.)
-	"footer" => '<a href="https://github.com/mitchellurgero/bootforums">Get BootForums from GitHub today!</a>', //Footer will be displayed under the copyright text, you can put links, or whatever you want in here.
-	"registration" => true, //Allow or disallow public registration (Disabled by default, for now, just change to true to enable.)
-	"perPage" => 20, //Default amount of threads to show per page. (Index only.)
-	"perPageThread" => 10, //Default reply amount to show in each thread (View mode)
-	"allowNewThreads" => true, //Enable or disable the New Post button on index page, this will also disable cURL posts!
-	"theme" => "cosmo"//Select a theme: orig, cyborg, dark, journal, superhero, readable, flat, or cosmo.
-	);
+    "admins" => array(""), //Username(s) of the account you want to be admin(**Register this account FIRST) This user will have access to locking threads, or deleting them even if they do not own it. Leave as just "" for no admin accounts.
+    "title"=>"BootForums", //Title for the forums
+    "desc"=>"Open Source Bootstrap themed forum for php 5.6+ - Flat file, no Database required!", //Description for the site in html (Not displayed on page, but in search engines.)
+    "home"=> "./", //Home page URL. to go back to home of forums: "./";
+    //Data folders must NOT be the same folders, please follow a similar structure to what I have below. (/path/to/data/users & /path/to/data/threads)
+    "user_data" => "/var/forum_data/users", //Folder to store user data in, make sure to give proper permissions (0744) and the owner of the folder must be apache's user (Or nginx's user)
+    "thread_data" => "/var/forum_data/threads", //Folder to store thread data in, make sure to give proper permissions (0744) and the owner of the folder must be apache's user (Or nginx's user)
+    "log" => "/var/forum_data/forums.log",
+    //Captcha Settings for REGISTRATION page
+    "captcha" => array( //Configure Captcha settings for registration.
+        'min_length' => 4,
+        'max_length' => 8,
+        'backgrounds' => array('backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png'),
+        'fonts' => array('fonts/times_new_yorker.ttf'),
+        'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
+        'min_font_size' => 15,
+        'max_font_size' => 20,
+        'color' => '#666',
+        'angle_min' => 0,
+        'angle_max' => 20,
+        'shadow' => true,
+        'shadow_color' => '#fff',
+        'shadow_offset_x' => -1,
+        'shadow_offset_y' => 1
+    ),
+    //Captcha settings for LOGIN page
+    "captchaLoginForce" => true, //Forces captcha for ALL logins whether they got the password wrong or not.
+    "captchaLogin" => array( //Configure Captcha settings for login.
+        'min_length' => 4,
+        'max_length' => 6,
+        'backgrounds' => array('backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png','backgrounds/45-degree-fabric.png'),
+        'fonts' => array('fonts/times_new_yorker.ttf'),
+        'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
+        'min_font_size' => 15,
+        'max_font_size' => 20,
+        'color' => '#666',
+        'angle_min' => 0,
+        'angle_max' => 20,
+        'shadow' => true,
+        'shadow_color' => '#fff',
+        'shadow_offset_x' => -1,
+        'shadow_offset_y' => 1
+    ),
+    "ssl" => true, //Force an SSL connection for Forums
+    "announce" => "This forum is currently in beta, but please enjoy your stay!", //Announcement to show on home page. set to "" if you want to disable. Supports HTML if needed.
+    "copyright" => 'Powered by <a href="https://github.com/mitchellurgero/bootforums">BootForums</a><br />Copyright &copy; URGERO.ORG 2016', //Copyright footer, can be anything, don't be shy. (Copyright is centered in page.)
+    "footer" => '<a href="https://github.com/mitchellurgero/bootforums">Get BootForums from GitHub today!</a>', //Footer will be displayed under the copyright text, you can put links, or whatever you want in here.
+    "registration" => true, //Allow or disallow public registration (Disabled by default, for now, just change to true to enable.)
+    "perPage" => 20, //Default amount of threads to show per page. (Index only.)
+    "perPageThread" => 10, //Default reply amount to show in each thread (View mode)
+    "allowNewThreads" => true, //Enable or disable the New Post button on index page, this will also disable cURL posts!
+    "theme" => "cosmo"//Select a theme: orig, cyborg, dark, journal, superhero, readable, flat, or cosmo.
+);
